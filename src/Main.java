3792bf654dcd45308a1fb50488d7ddc34cc1cfac
PaulMahar <paulmahar@outlook.com>
2024-09-16 17:00:51 -0300

    Initial push

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         theirGrade = sc.next();
 
+        if (theirGrade = letterGrades[0]){
+            System.out.println("Crushed it! You got an" + numberGrades[0]);
+        }
+        else if (theirGRade = letterGrades[1]){
+            System.out.println("Well done, you got an" + numberGrades[1]);
+        }
+        else if (theirGRade = letterGrades[2]){
+            System.out.println("A bit more studying wouldnt hurt, but " + numberGrades[2] + "'s get degrees!");
+        }
+        else if (theirGRade = letterGrades[3]){
+            System.out.println("Better luck next time! Your grade is: " + numberGrades[3]);
+        }
+        else if (theirGRade = letterGrades[4]){
+            System.out.println("Oh damn, it takes still to do that poorly. Enjoy your " + numberGrades[4]);
+        }
 
     }
 }
