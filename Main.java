a56c0a82580bb9666aaaf3a74d60b7cc9a4f5bf1
PaulMahar <paulmahar@outlook.com>
2024-09-16 16:10:59 -0300

    Initial push

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String[] letterGrades = {"A", "B", "C", "D", "F"};
+        int[] numberGrades = {4,3,2,1,0};
+
+        System.out.print("Please enter your grade: ");
+        Scanner sc = new Scanner(System.in);
+        theirGrade = sc.next();
+
+
+    }
+}
